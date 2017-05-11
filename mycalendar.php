3af3f0dca69be4168338302d721e9f160b11adc4
Anton Iun <nah7c00@gmail.com>
2017-05-12 03:32:44 +0800

    Develop new table design

--- mycalendar.php
+++ mycalendar.php
@@ -1,142 +1,141 @@
 <?php
 class myCalendar {
+	//determine class variables
+	private $defaultYear;
+	private $defaultMonth;
+	private $defaultDay;
 	private $language;
-	private $year;
-	private $month;
-	private $day;
-	private $serviceDays;
-	private $calendarMonth;
-	private $headings;
-	private $calendar;
-	public function __construct($year, $month, $day, $language) {
-		$this->year = $year;
-		$this->month = $month;
-		$this->day = $day;
+	private $header;
+	private $weedDays;
+	private $events;
+	private $table;
+
+	//determine class constructor
+	public function __construct($year, $month, $day, $language, $events) {
+		$this->defaultYear = $year;
+		$this->defaultMonth = $month;
+		$this->defaultDay = $day;
 		$this->language = $language;
-	}
-	public function update($serviceDays) {
-		$this->serviceDays = $serviceDays;
-	}
-	public function localization() {
+		$this->events = $events;
+
 		// table headings
 		switch($this->language) {
 			case 0:
-				$this->headings = array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
-				switch($this->month) {
-					case 1: $this->calendarMonth = 'January '.$this->year;
+				$this->weedDays = array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
+				switch($this->defaultMonth) {
+					case 1: $this->header = 'January '.$this->defaultYear;
 						break;
-					case 2: $this->calendarMonth = 'Febrary '.$this->year;
+					case 2: $this->header = 'Febrary '.$this->defaultYear;
 						break;
-					case 3: $this->calendarMonth = 'March '.$this->year;
+					case 3: $this->header = 'March '.$this->defaultYear;
 						break;
-					case 4: $this->calendarMonth = 'April '.$this->year;
+					case 4: $this->header = 'April '.$this->defaultYear;
 						break;
-					case 5: $this->calendarMonth = 'May '.$this->year;
+					case 5: $this->header = 'May '.$this->defaultYear;
 						break;
-					case 6: $this->calendarMonth = 'June '.$this->year;
+					case 6: $this->header = 'June '.$this->defaultYear;
 						break;
-					case 7: $this->calendarMonth = 'July '.$this->year;
+					case 7: $this->header = 'July '.$this->defaultYear;
 						break;
-					case 8: $this->calendarMonth = 'August '.$this->year;
+					case 8: $this->header = 'August '.$this->defaultYear;
 						break;
-					case 9: $this->calendarMonth = 'September '.$this->year;
+					case 9: $this->header = 'September '.$this->defaultYear;
 						break;
-					case 10: $this->calendarMonth = 'October '.$this->year;
+					case 10: $this->header = 'October '.$this->defaultYear;
 						break;
-					case 11: $this->calendarMonth = 'November '.$this->year;
+					case 11: $this->header = 'November '.$this->defaultYear;
 						break;
-					case 12: $this->calendarMonth = 'December '.$this->year;
+					case 12: $this->header = 'December '.$this->defaultYear;
 						break;
 				}
 				break;
 			case 1:
-				$this->headings = array('星期日','星期一','星期二','星期三','星期四','星期五','星期六');
-				switch($this->month) {
-					case 1: $this->calendarMonth = $this->year.'年 一月';
+				$this->weedDays = array('日','一','二','三','四','五','六');
+				switch($this->defaultMonth) {
+					case 1: $this->header = $this->defaultYear.'年 一月';
 						break;
-					case 2: $this->calendarMonth = $this->year.'年 二月';
+					case 2: $this->header = $this->defaultYear.'年 二月';
 						break;
-					case 3: $this->calendarMonth = $this->year.'年 三月';
+					case 3: $this->header = $this->defaultYear.'年 三月';
 						break;
-					case 4: $this->calendarMonth = $this->year.'年 四月';
+					case 4: $this->header = $this->defaultYear.'年 四月';
 						break;
-					case 5: $this->calendarMonth = $this->year.'年 五月';
+					case 5: $this->header = $this->defaultYear.'年 五月';
 						break;
-					case 6: $this->calendarMonth = $this->year.'年 六月';
+					case 6: $this->header = $this->defaultYear.'年 六月';
 						break;
-					case 7: $this->calendarMonth = $this->year.'年 七月';
+					case 7: $this->header = $this->defaultYear.'年 七月';
 						break;
-					case 8: $this->calendarMonth = $this->year.'年 八月';
+					case 8: $this->header = $this->defaultYear.'年 八月';
 						break;
-					case 9: $this->calendarMonth = $this->year.'年 九月';
+					case 9: $this->header = $this->defaultYear.'年 九月';
 						break;
-					case 10: $this->calendarMonth = $this->year.'年 十月';
+					case 10: $this->header = $this->defaultYear.'年 十月';
 						break;
-					case 11: $this->calendarMonth = $this->year.'年 十一月';
+					case 11: $this->header = $this->defaultYear.'年 十一月';
 						break;
-					case 12: $this->calendarMonth = $this->year.'年 十二月';
+					case 12: $this->header = $this->defaultYear.'年 十二月';
 						break;
 				}
 				break;
 		}
 	}
-	private function previousMonth() {
-		if($this->month > 1) {
-			$newYear = $this->year;
-			$newMonth = $this->month-1;
-			$newDay = $this->day;
+
+	//create GET request of previous month
+	private function requestPreviousMonth() {
+		//if not January
+		if($this->defaultMonth > 1) {
+			$year = $this->defaultYear;
+			$month = $this->defaultMonth-1;
+			$day = $this->defaultDay;
 		} else {
-			$newYear = $this->year-1;
-			$newMonth = 12;
-			$newDay = $this->day;
+			$year = $this->defaultYear-1;
+			$month = 12;
+			$day = $this->defaultDay;
 		}
-		return "year=$newYear&month=$newMonth&day=$newDay";
+		return "?year=$year&month=$month&day=$day";
 	}
-	private function nextMonth() {
-		if($this->month < 12) {
-			$newYear = $this->year;
-			$newMonth = $this->month+1;
-			$newDay = $this->day;
+
+	//create GET request of next month
+	private function requestNextMonth() {
+		//if not December
+		if($this->defaultMonth < 12) {
+			$year = $this->defaultYear;
+			$month = $this->defaultMonth+1;
+			$day = $this->defaultDay;
 		} else {
-			$newYear = $this->year+1;
-			$newMonth = 1;
-			$newDay = $this->day;
+			$year = $this->defaultYear+1;
+			$month = 1;
+			$day = $this->defaultDay;
 		}
-		return "year=$newYear&month=$newMonth&day=$newDay";
+		return "?year=$year&month=$month&day=$day";
 	}
+
 	public function draw() {
 		// calendar header
-		$this->calendar = '<div class="calendar-head">';
-		$this->calendar.= '<a href="?';
-		$this->calendar.= $this->previousMonth();
-		$this->calendar.= '"><img src="left_arrow.png" alt="previous" width=25px></a>';
-		$this->calendar.= '<div>';
-		$this->calendar.= $this->calendarMonth;
-		$this->calendar.= '</div>';
-		$this->calendar.= '<a href="?';
-		$this->calendar.= $this->nextMonth();
-		$this->calendar.= '"><img src="right_arrow.png" alt="next" width=25px></a>';
-		$this->calendar.= '</div>';
-		
-		// draw table 
-		$this->calendar.= '<table class="calendar">';
-		$this->calendar.= '<tr class="calendar-row">';
-		$this->calendar.= '<td class="calendar-day-head">';
-		$this->calendar.= '<div class="day-head">';
-		$this->calendar.= implode('</div></td><td class="calendar-day-head"><div class="day-head">',$this->headings);
-		$this->calendar.= '</div>';
-		$this->calendar.= '</td>';
-		$this->calendar.= '</tr>';
+		$this->table = '<table id="calendar">';
+		$this->table.= '<tr class="calendar-head">';
+		$this->table.= '<td id="calendar-prev"><a href="'.$this->requestPreviousMonth().'">Prev.</a></td>';
+		$this->table.= '<td colspan="5" id="calendar-month">'.$this->header.'</td>';
+		$this->table.= '<td id="calendar-next"><a href="'.$this->requestNextMonth().'">Next</a></td>';
+		$this->table.= '</tr>';
+
+		// names of week days
+		$this->table.= '<tr class="calendar-head">';
+		$this->table.= '<td class="calendar-day-head">';
+		$this->table.= implode('</td><td class="calendar-day-head">', $this->weedDays);
+		$this->table.= '</td>';
+		$this->table.= '</tr>';
 		
 		/* days and weeks vars now ... */
-		$running_day = date('w',mktime(0,0,0,$this->month,1,$this->year));
-		$days_in_month = date('t',mktime(0,0,0,$this->month,1,$this->year));
+		$running_day = date('w',mktime(0,0,0,$this->defaultMonth,1,$this->defaultYear));
+		$days_in_month = date('t',mktime(0,0,0,$this->defaultMonth,1,$this->defaultYear));
 		$days_in_this_week = 1;
 		$day_counter = 0;
 		$dates_array = array();
 		
 		/* row for week one */
-		$this->calendar.= '<tr class="calendar-row">';
+		$this->table.= '<tr class="calendar-row">';
 		
 		/* print "blank" days until the first of the current week */
 		for($x = 0; $x < $running_day; $x++):
@@ -146,26 +145,26 @@ class myCalendar {
 
 		/* keep going with days.... */
 		for($list_day = 1; $list_day <= $days_in_month; $list_day++):
-			$this->calendar.= '<td class="calendar-day';
+			$this->table.= '<td class="calendar-day';
 			$day = str_pad($list_day, 2, '0', STR_PAD_LEFT);
-			$string = $this->year.'-'.$this->month.'-'.$day;
+			$string = $this->defaultYear.'-'.$this->defaultMonth.'-'.$day;
 
 			/* add in the day number */
-			if(in_array($string, $this->serviceDays)) {
+			if(in_array($string, $this->events)) {
 				$this->calendar.= ' active';
 			}
-			if($day == $this->day) {
-				$this->calendar.= ' selected';
+			if($day == $this->defaultDay) {
+				$this->table.= ' selected';
 			}
-			$this->calendar.= '">';
-			$this->calendar.= '<div class="day-number" onclick="location.href=\'';
-			$this->calendar.= "?year=$this->year&month=$this->month&day=$day";
-			$this->calendar.= '\'">'.$day.'</div>';
-			$this->calendar.= '</td>';
+			$this->table.= '">';
+			$this->table.= '<a href="';
+			$this->table.= "?year=$this->defaultYear&month=$this->defaultMonth&day=$day";
+			$this->table.= '" class="day-number">'.$day.'</a>';
+			$this->table.= '</td>';
 			if($running_day == 6):
-				$this->calendar.= '</tr>';
+				$this->table.= '</tr>';
 				if(($day_counter+1) != $days_in_month):
-					$calendar.= '<tr class="calendar-row">';
+					$this->table.= '<tr class="calendar-row">';
 				endif;
 				$running_day = -1;
 				$days_in_this_week = 0;
@@ -176,21 +175,21 @@ class myCalendar {
 		/* finish the rest of the days in the week */
 		if($days_in_this_week < 8):
 			for($x = 1; $x <= (8 - $days_in_this_week); $x++):
-				$this->calendar.= '<td class="calendar-day-np"> </td>';
+				$this->table.= '<td class="calendar-day-np"> </td>';
 			endfor;
 		endif;
 		
 		/* final row */
-		$this->calendar.= '</tr>';
+		$this->table.= '</tr>';
 		
 		/* end the table */
-		$this->calendar.= '</table>';
+		$this->table.= '</table>';
 		
 		/* all done, return result */
-		return $this->calendar;
+		return $this->table;
 	}
 	public function show() {
-		echo $this->calendar;
+		echo $this->table;
 	}
 }
 ?>
