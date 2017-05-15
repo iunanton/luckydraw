748d9585fb9a6f36c77b180710958cc13f034f24
Anton Iun <nah7c00@gmail.com>
2017-05-15 17:56:42 +0800

    Show today's appointments

--- private/summary.php
+++ private/summary.php
@@ -1,10 +1,23 @@
+<?php
+	require_once('../constant.php');
+	
+	if(isset($_GET['lang'])) {
+		$global_lang = $_GET['lang'];
+	} else {
+		$global_lang = EN;
+	}
+
+	$global_page = basename(__FILE__, '.php');
+
+	require_once('../class/mydatabase.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Lucky Draw Studio - Summary</title>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-05-15T16:25:35+0800" >
+<meta name="date" content="2017-05-15T17:55:53+0800" >
 <meta name="copyright" content="">
 <meta name="keywords" content="">
 <meta name="description" content="">
@@ -19,6 +32,17 @@
 </style>
 </head>
 <body>
-For staff only
+For staff only<br><br>
+	<?php
+		echo "Today is ".date("d M, Y")."<br>";
+		echo "<br>";
+		$conn = new myDatabase();
+		$appointments = $conn->getAppointments(TODAY);
+		echo sizeof($appointments)." appointment(s) was founded:<br><br>";
+		foreach ($appointments as $appointment) {
+			echo $appointment['id']."|".$appointment['time']."|".$appointment['name']."|".$appointment['phone']."<br>";	
+		}
+		//$content = $conn->getNews($global_lang);
+	?>
 </body>
 </html>
