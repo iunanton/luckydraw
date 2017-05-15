b513b8ee8ece163fddbd00c610bce0b4b77228ec
Anton Iun <nah7c00@gmail.com>
2017-05-15 16:46:38 +0800

    Create summary.php file

---
+++ private/summary.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lucky Draw Studio - Summary</title>
+<meta name="generator" content="Bluefish 2.2.7" >
+<meta name="author" content="Anton Yun" >
+<meta name="date" content="2017-05-15T16:25:35+0800" >
+<meta name="copyright" content="">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+<link href="" rel="stylesheet" type="text/css">
+<style type="text/css">
+
+</style>
+</head>
+<body>
+For staff only
+</body>
+</html>
