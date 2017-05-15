b0aa389009c603736d6eb1352506ce97fabf20e3
Anton Iun <nah7c00@gmail.com>
2017-05-16 05:23:26 +0800

    Add news.php

---
+++ private/news.php
@@ -0,0 +1,66 @@
+<?php
+	require_once('../constant.php');
+	
+	if(isset($_GET['lang'])) {
+		$global_lang = $_GET['lang'];
+	} else {
+		$global_lang = EN;
+	}
+
+	$global_page = basename(__FILE__, '.php');
+
+	require_once('../class/mydatabase.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		include('view/title.php');
+	?>
+<meta name="generator" content="Bluefish 2.2.7" >
+<meta name="author" content="Anton Yun" >
+<meta name="date" content="2017-05-16T05:22:50+0800" >
+<meta name="copyright" content="">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+<link href="../style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+
+</style>
+</head>
+<body>
+	<?php
+		include('view/header.php');
+	?>
+	<?php
+		include('view/navigation_bar.php');
+	?>
+	<div id="wrapper">
+		<div id="wrapper-header">
+			<?php
+				switch($global_lang) {
+					case EN:
+						$header = "News";
+						break;
+					case ZH:
+						$header = "最新消息";
+						break;
+				}
+			?>
+			<h1><?= $header; ?></h1>
+		</div>
+		<div id="wrapper-content">
+			Today is <?=date("d M, Y"); ?><br><br>
+			Add news.<br><br>
+		</div>
+	</div>
+	<?php
+		include('view/footer.php');
+	?>
+</body>
+</html>
