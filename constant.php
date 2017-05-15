cb9a390a85e5d51dfd823f08f3bddd449f953a82
Anton Iun <nah7c00@gmail.com>
2017-05-15 17:34:41 +0800

    Add date constants

--- constant.php
+++ constant.php
@@ -5,4 +5,8 @@
 	define("KR", 4, false);
 	define("JP", 5, false);
 	define("RU", 6, false);
+	define("TODAY", date("Y-m-d"), false);
+	define("YEAR", date("Y"), false);
+	define("MONTH", date("m"), false);
+	define("DAY", date("d"), false);
 ?>
