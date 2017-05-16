013e5e18b1286c1632cdf812961a47986ee4ab9e
Anton Iun <nah7c00@gmail.com>
2017-05-17 06:04:38 +0800

    Add test-booking and videos to provate nav-bar

--- private/view/navigation_bar.php
+++ private/view/navigation_bar.php
@@ -4,11 +4,15 @@
 			$summary = "SUMMARY";
 			$news = "NEWS";
 			$rapid_test = "RAPID TEST";
+			$test_booking = "TEST BOOKING";
+			$videos = "VIDEOS";
 			break;
 		case ZH:
 			$summary = "SUMMARY";
 			$news = "最新消息";
 			$rapid_test = "快速測試";
+			$test_booking = "預約測試";
+			$videos = "相關影片";
 			break;
 	}
 	switch($global_page) {
@@ -21,10 +25,18 @@
 		case "rapid_test":
 			$rapid_test_class_modifier = " selected";
 			break;
+		case "booking":
+			$booking_class_modifier = " selected";
+			break;
+		case "videos":
+			$videos_class_modifier = " selected";
+			break;
 	}
 ?>
 <nav>
 	<div class="nav-link<?=$summary_class_modifier; ?>" id="summary"><a href="summary.php?lang=<?=$global_lang ?>"><?= $summary; ?></a></div>
 	<div class="nav-link<?=$news_class_modifier; ?>" id="news"><a href="news.php?lang=<?=$global_lang ?>"><?= $news; ?></a></div>
-	<div class="nav-link<?=$rapid_test_class_modifier; ?>" id="rapid_test"><a href="rapid_test.php?lang=<?=$global_lang ?>"><?= $rapid_test; ?></a></div>
+	<div class="nav-link<?=$rapid_test_class_modifier; ?>" id="rapid-test"><a href="rapid_test.php?lang=<?=$global_lang ?>"><?= $rapid_test; ?></a></div>
+	<div class="nav-link<?=$booking_class_modifier; ?>" id="test-booking"><a href="booking.php?lang=<?=$global_lang ?>"><?= $test_booking; ?></a></div>
+	<div class="nav-link<?=$videos_class_modifier; ?>" id="videos"><a href="videos.php?lang=<?=$global_lang ?>"><?=$videos; ?></a></div>
 </nav>
