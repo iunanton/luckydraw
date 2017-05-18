07ed5918c3acebc0b20c83ad8df930be23289775
Anton Iun <nah7c00@gmail.com>
2017-05-18 23:52:15 +0800

    Add time_slots table autofill script

---
+++ class/fill_time_slots_table.php
@@ -0,0 +1,11 @@
+<?php
+	$max_day = 31;
+	echo basename(__FILE__, ".php")." processing...<br>";
+	require_once('mydatabase.php');
+	$conn = new myDatabase();
+	for($day = 1; $day <= $max_day; $day++) {
+		$date = "2017-07-".str_pad($day, 2, '0', STR_PAD_LEFT);
+		echo "Fill time slots for ".$date."<br>";
+		$conn->fillTimeSlotTable($date);
+	}
+?>
