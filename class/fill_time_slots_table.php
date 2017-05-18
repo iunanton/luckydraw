172727eb8ccd8e047fd1b0cdbe9407c57d5ad262
Anton Iun <nah7c00@gmail.com>
2017-05-19 01:43:22 +0800

    Change parameters

--- class/fill_time_slots_table.php
+++ class/fill_time_slots_table.php
@@ -4,7 +4,7 @@
 	require_once('mydatabase.php');
 	$conn = new myDatabase();
 	for($day = 1; $day <= $max_day; $day++) {
-		$date = "2017-07-".str_pad($day, 2, '0', STR_PAD_LEFT);
+		$date = "2017-05-".str_pad($day, 2, '0', STR_PAD_LEFT);
 		echo "Fill time slots for ".$date."<br>";
 		$conn->fillTimeSlotTable($date);
 	}
