ee8e37273bf1239774de84b28b08fce0de0187d2
Anton Iun <nah7c00@gmail.com>
2017-05-19 23:07:29 +0800

    Add content to rapid_test.php

--- rapid_test.php
+++ rapid_test.php
@@ -17,7 +17,7 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-05-19T06:27:47+0800" >
+<meta name="date" content="2017-05-19T23:07:08+0800" >
 <meta name="copyright" content="">
 <meta name="keywords" content="">
 <meta name="description" content="">
@@ -55,7 +55,35 @@
 			<h1><?= $header; ?></h1>
 		</div>
 		<div id="wrapper-content">
+			<del>Switch language and table style will be implemented later..</del>
 			<p>A-Backup Testing Centre opens Monday to Saturday and Public Holidays need reservation. We provided Anonymous Rapid HIV Test. Only take a drop of blood and the result will come out within 15 minutes. All test are conducted in private and result will remain Anonymous.</p>
+			<p>
+Rapid-Test
+愛滋病測試 / HIV測試
+「幸運抽獎工作室」Lucky Draw Studio 逢星期一至日及公眾假期提供可即日預約無需等幾日的不記名愛滋病測試及其他性病測試，只需拮一下手指，15分鐘就有結果！所有測試過程及結果絕對保密。
+本會未來將會申請政府資助，但現時只靠捐款運作，而捐款全數用於人工、租金、試劑等開支，每日營運開支~$600，預計每人捐$200~300，如有經濟困難請開聲。若捐款持續不足維持成本將會停止服務。			
+			
+			</p>
+			<img src="images/tail_3.jpg" alt="test" height="160">熱線電話：<strong>5405 6631</strong>
+			<table class="content-table">
+				<tr>
+					<th>SUN</th>
+					<th>MON</th>
+					<th>TUE</th>
+					<th>WED</th>
+					<th>THU</th>
+					<th>FRI</th>
+					<th>SAT</th>
+				</tr>
+				<tr>
+					<td colspan="7">16:30-20:30<br>可親臨本中心即場安排測試<br>(無預約停止派籌時間 20:30)</td>
+				</tr>
+				<tr>
+					<td colspan="7">服務時間 Service Hour 16:30 - 20:30<br>請電 Tel 5405 6631或 <a href="booking.php?lang=<?=$global_lang; ?>">網上預約</a></td>				
+				</tr>
+			</table>
+			最快捷方便>>><a href="booking.php?lang=<?=$global_lang; ?>">按此網上預約</a><br>
+			觀塘鴻圖道58號金凱工業大廈6樓25室
 		</div>
 	</div>
 	<?php
