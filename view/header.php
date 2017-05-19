15d15513ef3b22cc48df06ecddc5c6bc0a16916e
Anton Iun <nah7c00@gmail.com>
2017-05-19 22:46:15 +0800

    Add language-bar

--- view/header.php
+++ view/header.php
@@ -1,5 +1,11 @@
 <header>
-	<img src="logo.jpg" alt="logo" height="100px">
-	<a href="?lang=<?=EN; ?>">English</a>
-	<a href="?lang=<?=ZH; ?>">简体</a>	
+	<div class="header-image">
+		<img src="logo.jpg" alt="logo" height="100px">
+	</div>
+	<div class="language-bar">
+		<ul>
+			<li><a href="?lang=<?=EN; ?>">English</a></li>
+			<li><a href="?lang=<?=ZH; ?>">简体</a></li>
+		</ul>
+	</div>
 </header>
