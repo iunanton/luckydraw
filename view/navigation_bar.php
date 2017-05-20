855102149aabf8cb88382f197e592a6e0e50be38
Anton Iun <nah7c00@gmail.com>
2017-05-21 02:59:19 +0800

    Add simplified chinese to title and nav-bar

--- view/navigation_bar.php
+++ view/navigation_bar.php
@@ -14,11 +14,21 @@
 			$news = "最新消息";
 			$rapid_test = "快速測試";
 			$test_booking = "預約測試";
-			$free_condom = "無標題文件";
+			$free_condom = "免費安全套";
 			$videos = "相關影片";
 			$hiv_pos = "HIV 陽性";
 			$join_us = "加入我們";
 			$about_US = "關於我們";
+			break;
+		case CH:
+			$news = "最新消息";
+			$rapid_test = "快速测试";
+			$test_booking = "预约测试";
+			$free_condom = "免费安全套";
+			$videos = "相关影片";
+			$hiv_pos = "HIV 阳性";
+			$join_us = "加入我们";
+			$about_US = "关于我们";
 			break;
 	}
 	switch($global_page) {
--- view/title.php
+++ view/title.php
@@ -52,7 +52,7 @@
 					$title = "幸運抽獎工作室 - Confirmation";
 					break;
 				case "free_condom":
-					$title = "幸運抽獎工作室 - 無標題文件";
+					$title = "幸運抽獎工作室 - 免費安全套";
 					break;
 				case "videos":
 					$title = "幸運抽獎工作室 - 相關影片";
@@ -61,13 +61,47 @@
 					$title = "幸運抽獎工作室 - HIV 陽性";
 					break;
 				case "join_us":
-					$title = "幸運抽獎工作室 - 關於我們";
+					$title = "幸運抽獎工作室 - 加入我們";
 					break;
 				case "about_us":
 					$title = "幸運抽獎工作室 - 關於我們";
 					break;
 			}
 			break;
+		case CH:
+			switch($global_page) {
+				case "news":
+					$title = "幸运抽奖工作室 - 最新消息";
+					break;
+				case "rapid_test":
+					$title = "幸运抽奖工作室 - 快速测试";
+					break;
+				case "booking":
+					$title = "幸运抽奖工作室 - 预约测试";
+					break;
+				case "booking_form":
+					$title = "幸运抽奖工作室 - Booking Form";
+					break;
+				case "confirmation":
+					$title = "幸运抽奖工作室 - Confirmation";
+					break;
+				case "free_condom":
+					$title = "幸运抽奖工作室 - 免费安全套";
+					break;
+				case "videos":
+					$title = "幸运抽奖工作室 - 相关影片";
+					break;
+				case "hiv_pos":
+					$title = "幸运抽奖工作室 - HIV 阳性";
+					break;
+				case "join_us":
+					$title = "幸运抽奖工作室 - 加入我们";
+					break;
+				case "about_us":
+					$title = "幸运抽奖工作室 - 关于我们";
+					break;
+			}
+			break;
 	}
 ?>
-<title><?=$title ?></title>
+<title><?=$title; ?></title>
