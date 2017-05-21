168a5b3716e0ff7f9bd974b61d115028e201bb4c
Anton Iun <nah7c00@gmail.com>
2017-05-21 19:29:23 +0800

    Make some text for whatsapp and line

--- view/footer.php
+++ view/footer.php
@@ -2,21 +2,21 @@
 	switch($global_lang) {
 		case EN:
 			$first_column = ARRAY("CENTER","About Us","Join Us","Staff");
-			$second_column = ARRAY("CONTACT","Tel.: (852) 5405 6631","E-mail: luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">luckydrawstudio</span>");
+			$second_column = ARRAY("CONTACT","Tel.: (852) 5405 6631","E-mail: luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Whatsapp: 5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Line ID: luckydrawstudio</span>");
 			$third_column = ARRAY("ADDRESS","Flat 25, 6/F,","Career And Kenson Industrial Mansion,","58 Hung To Rd,","Kwun Tong, HK");
 			$fourth_column = ARRAY("GO UP");
 			$copyright = "&copy Copyright By Lucky Draw Studio";
 			break;
 		case ZH:
 			$first_column = ARRAY("中心","關於我們","加入我們","工作人員");
-			$second_column = ARRAY("聯絡","電話：(852) 5405 6631","電郵地址：luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">luckydrawstudio</span>");
+			$second_column = ARRAY("聯絡","電話：(852) 5405 6631","電郵地址：luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Whatsapp: 5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Line ID: luckydrawstudio</span>");
 			$third_column = ARRAY("地址","香港觀塘鴻圖道58號","金凱工業大廈","6樓 25室");
 			$fourth_column = ARRAY("回頂");
 			$copyright = "&copy Copyright By 幸運抽獎工作室";
 			break;
 		case CH:
 			$first_column = ARRAY("中心","关于我们","加入我们","工作人员");
-			$second_column = ARRAY("联络","电话：(852) 5405 6631","电邮地址：luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">luckydrawstudio</span>");
+			$second_column = ARRAY("联络","电话：(852) 5405 6631","电邮地址：luckygrawkt@gmail.com","<img src=\"images/whatsapp.png\" alt=\"whatsapp\" height=\"36\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Whatsapp: 5405 6631</span>","<img src=\"images/line.png\" alt=\"whatsapp\" height=\"48\" style=\"vertical-align:middle\"><span style=\"vertical-align:middle\">Line ID: luckydrawstudio</span>");
 			$third_column = ARRAY("地址","香港觀塘鴻圖道58號","金凱工業大廈","6樓 25室");
 			$fourth_column = ARRAY("回顶");
 			$copyright = "&copy Copyright By 幸運抽獎工作室";
