c92c6275dc182f2ad7f76cd6e63e283647354717
Anton Iun <nah7c00@gmail.com>
2017-05-23 19:52:01 +0800

    Improve script to fill time slots

--- class/fill_time_slots_table.php
+++ class/fill_time_slots_table.php
@@ -1,11 +1,15 @@
 <?php
-	$max_day = 31;
-	echo basename(__FILE__, ".php")." processing...<br>";
-	require_once('mydatabase.php');
-	$conn = new myDatabase();
-	for($day = 1; $day <= $max_day; $day++) {
-		$date = "2017-06-".str_pad($day, 2, '0', STR_PAD_LEFT);
-		echo "Fill time slots for ".$date."<br>";
-		$conn->fillTimeSlotTable($date);
+	if(isset($_GET['date'])) {
+		$max_day = 31;
+		echo basename(__FILE__, ".php")." processing...<br>";
+		require_once('mydatabase.php');
+		$conn = new myDatabase();
+		for($day = 1; $day <= $max_day; $day++) {
+			$date = $_GET['date'].str_pad($day, 2, '0', STR_PAD_LEFT);
+			echo "Fill time slots for ".$date."<br>";
+			$conn->fillTimeSlotTable($date);
+		}
+	} else {
+		echo "Please open ".__FILE__."?date=YYY-MM";	
 	}
 ?>
