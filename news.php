00f72a060dbde7f4b360bbc6c2ccf957823ba8e9
Anton Iun <nah7c00@gmail.com>
2017-05-30 00:12:04 +0800

    Move styles to external file

--- news.php
+++ news.php
@@ -10,7 +10,7 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-05-30T00:04:15+0800" >
+<meta name="date" content="2017-05-30T00:11:47+0800" >
 <meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
 <meta name="description" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
@@ -21,103 +21,8 @@
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
 <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
-<link href="" rel="stylesheet" type="text/css">
+<link href="style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-
-	body {
-		margin: 0;
-		padding: 0;
-		font-family: sans-serif;
-		background-color: #FFFFFF;
-		color: #305D7C;
-	}
-	
-	.container {
-		display: block;
-	}
-	
-	a {
-		color: inherit;
-	}
-	
-	.language-bar ul {
-		list-style-type: none;
-		padding: 0;
-		margin: 0;
-	}
-	
-	.language-bar li {
-		display: inline-block;
-	}
-
-	.language-bar a {
-		display: inline-block;
-		padding: 5px 10px 5px 10px;	
-	}
-	
-	header {
-		padding: 10px 10px 10px 10px;
-	}
-
-	header img {
-		width: 100%;		
-	}
-	
-	nav ul {
-		list-style-type: none;
-		padding: 0;
-		margin: 0;
-		background-color: #FFA366;
-	}
-
-	nav li {
-		display: inline-block;
-		vertical-align: middle;
-		max-width: 25%;
-	}
-	
-	nav a {
-		display: inline-block;
-		padding: 10px 5px 10px 5px;	
-	}
-	
-	nav a:hover {
-		background-color: #FF6600;
-	}
-	
-	.wrapper {
-		padding-left: 10px;
-		padding-right: 10px;
-	}
-	
-	footer {
-		font-size: 12px;
-		background-color: #EEEEEE;
-		color: #305D7C;
-	}
-	
-	.footer-row {
-		display: block;
-		padding: 10px 10px 10px 10px;		
-		text-align: left;
-	}
-	
-	.footer-group {
-		display: block;
-		vertical-align: top;
-	}
-	
-	.footer-group ul {
-		list-style-type: none;
-		padding: 0;
-		margin: 0;	
-	}
-	
-	#footer-copyright {
-		font-size: 10px;		
-		background-color: #98D9D3;
-	}
-	
 </style>
 </head>
 <body>
