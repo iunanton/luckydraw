6b84eabcd61319fa82bfaa76bfac2d834acface8
Anton Iun <nah7c00@gmail.com>
2017-05-30 01:17:47 +0800

    Update

--- free_condom.php
+++ free_condom.php
@@ -1,15 +1,9 @@
+<!DOCTYPE html>
 <?php
 	require_once('constant.php');
-	if(isset($_GET['lang'])) {
-		$global_lang = $_GET['lang'];
-	} else {
-		$global_lang = ZH;
-	}
-	
+	require_once('class/mydatabase.php');
 	$global_page = basename(__FILE__, '.php');
-	
 ?>
-<!DOCTYPE html>
 <html>
 <head>
 	<?php
@@ -18,44 +12,48 @@
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
 <meta name="date" content="2017-05-23T15:58:25+0800" >
-<meta name="copyright" content="Lucky Draw Studio">
+<meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
 <meta name="description" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
+<meta name="theme-color" content="#FFA366" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
 <link href="style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 </style>
 </head>
 <body>
-	<?php
-		include('view/header.php');
-	?>
-	<?php
-		include('view/navigation_bar.php');
-	?>
-	<div id="wrapper">
-		<div id="wrapper-header">
-			<?php
-				switch($global_lang) {
-					case EN:
-						$header = "Free condom";
-						break;
-					case ZH:
-						$header = "無標題文件";
-						break;
-				}
-			?>
-			<h1><?= $header; ?></h1>
+	<div class="container">
+		<?php
+			include('view/header.php');
+		?>
+		<?php
+			include('view/navigation_bar.php');
+		?>
+		<div class="wrapper">
+			<div class="wrapper-header">
+				<?php
+					switch($global_lang) {
+						case EN:
+							$header = "Free condom";
+							break;
+						case ZH:
+							$header = "無標題文件";
+							break;
+					}
+				?>
+				<h1><?= $header; ?></h1>
+			</div>
+			<div class="wrapper-content">
+				<p>Coming soon...</p>
+			</div>
 		</div>
-		<div id="wrapper-content">
-			<p>Coming soon...</p>
-		</div>
+		<?php
+			include('view/footer.php');
+		?>
 	</div>
-	<?php
-		include('view/footer.php');
-	?>
 </body>
 </html>
