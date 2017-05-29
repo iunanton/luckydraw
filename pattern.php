4d01adf32359f68017be0d5e0f9adea4a939851c
Anton Iun <nah7c00@gmail.com>
2017-05-30 00:26:08 +0800

    Use style.css now

--- pattern.php
+++ pattern.php
@@ -9,7 +9,7 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-05-30T00:20:52+0800" >
+<meta name="date" content="2017-05-30T00:25:54+0800" >
 <meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="">
 <meta name="description" content="">
@@ -20,7 +20,7 @@
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
 <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
-<link href="style2.css" rel="stylesheet" type="text/css">
+<link href="style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 	
 </style>
