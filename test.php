18ce5ac6aa9ef26f4dd9627d0aec21676aee921b
Anton Iun <nah7c00@gmail.com>
2017-06-01 05:42:06 +0800

    Add viewport meta tag

--- test.php
+++ test.php
@@ -4,8 +4,8 @@
 <title></title>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-06-01T05:37:05+0800" >
-<meta name="copyright" content="">
+<meta name="date" content="2017-06-01T05:41:51+0800" >
+<meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="">
 <meta name="description" content="">
 <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
@@ -13,6 +13,7 @@
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
 <link href="" rel="stylesheet" type="text/css">
 <style type="text/css">
 	body {
