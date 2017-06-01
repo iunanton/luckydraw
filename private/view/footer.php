b6fc9677ed7cea0411994e8f57e1f2f6271dc1de
Anton Iun <nah7c00@gmail.com>
2017-06-01 19:13:50 +0800

    Update provat views to mobile version

--- private/view/footer.php
+++ private/view/footer.php
@@ -3,26 +3,26 @@
 		case EN:
 			$first_column = ARRAY("CENTER","Home");
 			$second_column = ARRAY("GO UP");
-			$copyright = "&copy Copyright By Lucky Draw Studio";
+			$copyright = "Copyright &copy 2016-".YEAR.' <a href="http://xiaodong.com.ru" style="color: inherit">XIAODONG IT Consulting</a>';
 			break;
 		case ZH:
 			$first_column = ARRAY("中心","首頁");
 			$second_column = ARRAY("GO UP");
-			$copyright = "&copy Copyright By 幸運抽獎工作室";
+			$copyright = "Copyright &copy 2016-".YEAR.' <a href="http://xiaodong.com.ru" style="color: inherit">XIAODONG IT Consulting</a>';
 			break;
 	}
 ?>
 <footer>
-	<div class="footer-row">
-		<div class="footer-column">
+	<div class="footer-row" id="footer-info">
+		<div class="footer-group">
 			<h3><?=$first_column[0]; ?></h3>
-			<ul>
-				<li><a href="../news.php?lang=<?=$global_lang; ?>"><?=$first_column[1]; ?></a></li>
+			<ul class="nav">
+				<li><a href="../news.php"><?=$first_column[1]; ?></a></li>
 			</ul>
 		</div>
-		<div class="footer-column">
-			<a href="#"><?=$second_column[0]; ?></a>
+		<div class="footer-group">
+			<h3><a href="#"><?=$second_column[0]; ?></a></h3>
 		</div>
 	</div>
-	<div class="footer-row" id="footer-copyright"><?=$copyright; ?></div>
+	<div class="footer-row" id="footer-copyright"><?=$copyright; ?></div>		
 </footer>
--- private/view/header.php
+++ private/view/header.php
@@ -1,4 +1,2 @@
 <header>
-	<a href="?lang=<?=EN; ?>">English</a>
-	<a href="?lang=<?=ZH; ?>">简体</a>	
 </header>
