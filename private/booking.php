dee6e010812bb2fcfc53d45d9b79485a5d5f5621
Anton Iun <nah7c00@gmail.com>
2017-06-01 20:10:48 +0800

    Update page to mobile version

--- private/booking.php
+++ private/booking.php
@@ -1,17 +1,11 @@
+<!DOCTYPE html>
 <?php
 	require_once('../constant.php');
-	
-	if(isset($_GET['lang'])) {
-		$global_lang = $_GET['lang'];
-	} else {
-		$global_lang = EN;
-	}
-
-	$global_page = basename(__FILE__, '.php');
-
+	require_once('../class/db.class.php');
 	require_once('../class/mydatabase.php');
+	$global_page = basename(__FILE__, '.php');
+	$db_conn = new db();
 ?>
-<!DOCTYPE html>
 <html>
 <head>
 	<?php
@@ -19,15 +13,17 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-05-19T00:44:29+0800" >
-<meta name="copyright" content="">
+<meta name="date" content="2017-06-01T19:55:40+0800" >
+<meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="">
 <meta name="description" content="">
 <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta name="theme-color" content="#FFA366" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
 <link href="../style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 
@@ -40,8 +36,8 @@
 	<?php
 		include('view/navigation_bar.php');
 	?>
-	<div id="wrapper">
-		<div id="wrapper-header">
+	<div class="wrapper">
+		<div class="wrapper-header">
 			<?php
 				switch($global_lang) {
 					case EN:
@@ -54,12 +50,12 @@
 			?>
 			<h1><?= $header; ?></h1>
 		</div>
-		<div id="wrapper-content">
+		<div class="wrapper-content">
 			<?php
 				$conn = new myDatabase();
 				$reservations = $conn->getAllReservations();
 				echo "<strong>".sizeof($reservations)." appointment(s)</strong> was founded:<br><br>";
-				echo '<table class="content-table">';
+				echo '<table class="sql-query">';
 				echo "<tr>";
 				echo "<th>id</th><th>date</th><th>time</th><th>name</th><th>phone</th><th>the booking received at</th>";
 				echo "</tr>";
