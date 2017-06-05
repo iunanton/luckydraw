d3187dc14b283f882a244d6d073c3b1a9d352fdb
Anton Iun <nah7c00@gmail.com>
2017-06-06 04:13:43 +0800

    Update

--- private/view/navigation_bar.php
+++ private/view/navigation_bar.php
@@ -37,18 +37,18 @@
 	<ul class="private-nav-bar">
 		<li class="nav-link<?=$summary_class_modifier; ?>">
 			<a href="summary.php"><?= $summary; ?></a>
-		</li>
-		<li class="nav-link<?=$news_class_modifier; ?>">
+		</li
+		><li class="nav-link<?=$news_class_modifier; ?>">
 			<a href="news.php"><?= $news; ?></a>
-		</li>
-		<li class="nav-link<?=$rapid_test_class_modifier; ?>">
+		</li
+		><li class="nav-link<?=$rapid_test_class_modifier; ?>">
 			<a href="rapid_test.php"><?= $rapid_test; ?></a>
-		</li>
-		<li class="nav-link<?=$booking_class_modifier; ?>">
+		</li
+		><li class="nav-link<?=$booking_class_modifier; ?>">
 			<a href="booking.php"><?= $test_booking; ?></a>
-		</li>
-		<li class="nav-link<?=$videos_class_modifier; ?>">
+		</li
+		><li class="nav-link<?=$videos_class_modifier; ?>">
 			<a href="videos.php"><?=$videos; ?></a>
 		</li>
-	<ul>
+	</ul>
 </nav>
