a81eb90e7844a636778c322bd07c73c24cdce384
Anton Iun <nah7c00@gmail.com>
2017-06-06 02:40:02 +0800

    Update

--- view/footer.php
+++ view/footer.php
@@ -17,16 +17,16 @@
 	}
 ?>
 <footer>
-	<div class="footer-row">
-		<div class="footer-column">
+	<div class="footer-row" id="footer-info">
+		<div class="footer-group">
 			<h3><?=$first_column[0]; ?></h3>
-			<ul>
-				<li><a href="about_us.php?lang=<?=$global_lang; ?>"><?=$first_column[1]; ?></a></li>
-				<li><a href="#"><?=$first_column[2]; ?></a></li>
-				<li><a href="private/summary.php?lang=<?=$global_lang; ?>"><?=$first_column[3]; ?></a></li>
+			<ul class="nav">
+				<li><a href="about_us.php"><?=$first_column[1]; ?></a></li>
+				<li><a href="join_us.php"><?=$first_column[2]; ?></a></li>
+				<li><a href="private/summary.php"><?=$first_column[3]; ?></a></li>
 			</ul>
 		</div>
-		<div class="footer-column">
+		<div class="footer-group">
 			<h3><?=$second_column[0]; ?></h3>
 			<ul>
 				<li><?=$second_column[1]; ?></li>
@@ -35,17 +35,17 @@
 				<li><?=$second_column[4]; ?></li>
 			</ul>
 		</div>
-		<div class="footer-column">
+		<div class="footer-group">
 			<h3><?=$third_column[0]; ?></h3>
-			<ul>
+			<ul class="address">
 				<li><?=$third_column[1]; ?></li>
 				<li><?=$third_column[2]; ?></li>
 				<li><?=$third_column[3]; ?></li>
 				<li><?=$third_column[4]; ?></li>
 			</ul>
 		</div>
-		<div class="footer-column">
-			<a href="#"><?=$fourth_column[0]; ?></a>
+		<div class="footer-group">
+			<h3><a href="#"><?=$fourth_column[0]; ?></a></h3>
 		</div>
 	</div>
 	<div class="footer-row" id="footer-copyright"><?=$copyright; ?></div>
