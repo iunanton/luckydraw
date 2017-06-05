e472e2064a9763042ec4cc2ec80a4f294d6d5caa
Anton Iun <nah7c00@gmail.com>
2017-06-06 04:54:59 +0800

    Update

--- private/news.php
+++ private/news.php
@@ -19,18 +19,38 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-06-06T04:50:17+0800" >
-<meta name="copyright" content="">
+<meta name="date" content="2017-06-06T04:54:25+0800" >
+<meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="">
 <meta name="description" content="">
 <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta name="theme-color" content="#FFA366" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
 <link href="../style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-
+	.input-field {
+		display: block;	
+	}
+	.input-field::after {
+		content: '';
+		clear: both;
+		display: block;	
+	}
+	.input-field label {
+		width: 100px;
+		float: left;
+		text-align: right;
+		margin-right: 10px;
+		display: block;
+	}
+	.input-field input[type=submit] {
+		float: left;
+		margin-left: 110px;	
+	}
 </style>
 </head>
 <body>
