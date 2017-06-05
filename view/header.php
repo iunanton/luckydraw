94255751d9eb749f779a48278a89916b39614de3
Anton Iun <nah7c00@gmail.com>
2017-06-06 03:29:02 +0800

    Update

--- view/header.php
+++ view/header.php
@@ -1,11 +1,3 @@
 <header>
-	<div class="header-image">
-		<img src="logo.jpg" alt="logo" height="100px">
-	</div>
-	<div class="language-bar">
-		<ul>
-			<li><a href="?lang=<?=ZH; ?>">繁體</a></li>
-			<li><a href="?lang=<?=EN; ?>">English</a></li>
-		</ul>
-	</div>
+	<img src="logo.jpg" alt="logo">
 </header>
