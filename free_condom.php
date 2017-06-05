a71899948fbc4fd92ffd06af1ba397bb80c279fa
Anton Iun <nah7c00@gmail.com>
2017-06-06 05:30:13 +0800

    Update

--- free_condom.php
+++ free_condom.php
@@ -12,7 +12,7 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-06-06T03:21:03+0800" >
+<meta name="date" content="2017-06-06T05:29:52+0800" >
 <meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
 <meta name="description" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
@@ -49,7 +49,20 @@
 				<h1><?= $header; ?></h1>
 			</div>
 			<div id="wrapper-content">
-				<p>Coming soon...</p>
+				<?php
+					switch($global_lang) {
+						case EN:
+							$caption = "While stock last";
+							break;
+						case ZH:
+							$caption = "送完即止";
+							break;	
+					}
+				?>
+				<div class="content-image">
+					<img src="images/IMG-20170605-WA0006.jpg" alt="condom">
+					<p><?=$caption; ?></p>
+				</div>
 			</div>
 		</div>
 		<?php
