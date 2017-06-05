9416da82b85c84cdad94834b9630b17ae3068809
Anton Iun <nah7c00@gmail.com>
2017-06-06 05:26:55 +0800

    Update

--- confirmation.php
+++ confirmation.php
@@ -28,7 +28,7 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-06-06T03:19:00+0800" >
+<meta name="date" content="2017-06-06T05:26:26+0800" >
 <meta name="copyright" content="XIAODONG IT Consulting">
 <meta name="keywords" content="">
 <meta name="description" content="">
@@ -41,8 +41,24 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=1" />
 <link href="style.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-	#test-confirmation .input-field {
+	.input-field {
 		display: block;	
+	}
+	.input-field::after {
+		content: '';
+		clear: both;
+		display: block;	
+	}
+	.input-field label {
+		width: 100px;
+		float: left;
+		text-align: right;
+		margin-right: 10px;
+		display: block;
+	}
+	.input-field input[type=submit] {
+		float: left;
+		margin-left: 110px;	
 	}
 </style>
 </head>
@@ -55,7 +71,7 @@
 			include('view/navigation_bar.php');
 		?>
 		<div class="wrapper">
-			<div id="wrapper-header">
+			<div class="wrapper-header">
 				<?php
 					switch($global_lang) {
 						case EN:
@@ -68,9 +84,17 @@
 				?>
 				<h1><?= $header; ?></h1>
 			</div>
-			<div id="wrapper-content">
+			<div class="wrapper-content">
 				<p>Your booking was confirmed successfully.</p>
-				<p><?= $info['date'].' '.$info['time'] ?></p>
+				<div class="input-field">
+					<label for="name-field">Date:</label>
+					<input type="text" id="name-field" name="date" value="<?= $info['date']; ?>" disabled="true">
+				</div>
+				<div class="input-field">
+					<label for="name-field">Time:</label>
+					<input type="text" id="name-field" name="time" value="<?= substr($info['time'], 0, 5); ?>" disabled="true">
+				</div>
+				<p><a href="rapid_test.php">Back 回頂</a></p>
 			</div>
 		</div>
 		<?php
