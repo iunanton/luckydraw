2f4c5b0dcd4bccad38ab3006bb4b37f08d70d219
Anton Iun <nah7c00@gmail.com>
2017-06-06 21:57:45 +0800

    Add new keywords

--- constant.php
+++ constant.php
@@ -14,8 +14,8 @@
 	define("OPERATION_TIME_FROM", "16:00:00", false);
 	define("OPERATION_TIME_TO", "21:00:00", false);
 	
-	$meta_keywords = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀";
-	$meta_description = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀";
+	$meta_keywords = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀, luckydraw, luckydrawhk, lucky draw studio";
+	$meta_description = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀, luckydraw, luckydrawhk, lucky draw studio";
 		
 	$locale = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
 	switch($locale) {
