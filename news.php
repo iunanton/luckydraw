ad5b3de429268a478db699d66b66a22fd65abb02
Anton Iun <nah7c00@gmail.com>
2017-06-06 21:21:50 +0800

    Move meta tags to constant.php file

--- news.php
+++ news.php
@@ -11,10 +11,10 @@
 	?>
 <meta name="generator" content="Bluefish 2.2.7" >
 <meta name="author" content="Anton Yun" >
-<meta name="date" content="2017-06-06T05:43:21+0800" >
+<meta name="date" content="2017-06-06T21:15:12+0800" >
 <meta name="copyright" content="XIAODONG IT Consulting">
-<meta name="keywords" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
-<meta name="description" content="愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀">
+<meta name="keywords" content="<?=$meta_keywords; ?>">
+<meta name="description" content="<?=$meta_description; ?>">
 <meta name="theme-color" content="#FFA366" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
