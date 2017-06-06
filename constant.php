168aea3967ae11ef042cf1d243bbc4331ab8772a
Anton Iun <nah7c00@gmail.com>
2017-06-06 21:13:21 +0800

    Add meta tag variables

--- constant.php
+++ constant.php
@@ -13,6 +13,10 @@
 	define("END_OF_BOOKING_TIME", "16:00:00", false);
 	define("OPERATION_TIME_FROM", "16:00:00", false);
 	define("OPERATION_TIME_TO", "21:00:00", false);
+	
+	$meta_keywords = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀";
+	$meta_description = "愛滋病測試，AIDS測試，hiv測試，愛滋測試九龍，AIDS測試九龍，hiv測試九龍，aids test kowloon,hiv test kowloon,aids test, hiv test,梅毒測試，syphilis test,梅毒測試，性病測試，STD test,STI test,heterosexual,異性戀";
+	
 	$locale = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
 	switch($locale) {
 		case "en-US": $global_lang = EN;
