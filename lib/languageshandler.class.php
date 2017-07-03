495dd997fcd8033bb24d3c9285a05483db85407d
Anton Iun <nah7c00@gmail.com>
2017-07-04 04:56:09 +0900

    Add getShort() method

--- lib/languageshandler.class.php
+++ lib/languageshandler.class.php
@@ -7,6 +7,15 @@
 			$this->db_conn = new dbConnector();
 		}
 		
+		public function getShort() {
+			$sql = "SELECT id, short";
+			$sql.= " FROM languages";
+			$stmt = $this->db_conn->pdo->prepare($sql);
+			$stmt->execute();
+			$lang = $stmt->fetchAll(PDO::FETCH_KEY_PAIR);
+			return $lang;
+		}
+		
 		public function get() {
 			$sql = "SELECT id, lang";
 			$sql.= " FROM languages";
