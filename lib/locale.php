78a92ba35c09d6fde3808a6eca352c6582e7089e
Anton Iun <nah7c00@gmail.com>
2017-07-17 06:45:23 +0800

    Delete echo

--- lib/locale.php
+++ lib/locale.php
@@ -5,7 +5,6 @@
 		$langs = array();
 		$preferred = array('en', 'zh', 'cn', 'kr');
 		if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-			echo $_SERVER['HTTP_ACCEPT_LANGUAGE']."<br>";
 			preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $lang_parse);
 			if (count($lang_parse[1])) {
 				$langs = array_combine($lang_parse[1], $lang_parse[4]);
@@ -16,7 +15,6 @@
 			}
 		}
 		$intersect = array_values(array_intersect(array_keys($langs), $preferred));
-		
 		if (isset($intersect[0])) {
 			$global_lang = $intersect[0];
 		} else {
