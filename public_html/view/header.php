3b75ebf2c2c58568e8df0297ad990ea46df5f0db
Anton Iun <nah7c00@gmail.com>
2017-07-18 05:08:41 +0800

    Delete Simplified Chinese and Korean

--- public_html/view/header.php
+++ public_html/view/header.php
@@ -3,7 +3,5 @@
 	<ul class="lang-bar">
 		<li><a href="/en/<?=$global_page; ?>">English</a></li>
 		<li><a href="/zh/<?=$global_page; ?>">繁體</a></li>
-		<li><a href="/cn/<?=$global_page; ?>">简体</a></li>
-		<li><a href="/kr/<?=$global_page; ?>">한국어</a></li>
 	</ul>
 </header>
