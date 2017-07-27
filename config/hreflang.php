53f28f816d71b5a8f901c536e79cd9473388e15d
Anton Iun <nah7c00@gmail.com>
2017-07-27 16:05:57 +0800

    Fix bug with lang tag

--- config/hreflang.php
+++ config/hreflang.php
@@ -1,6 +1,6 @@
 <?php
 	$HTTP_HOST = $_SERVER['HTTP_HOST'];
 ?>
-<link rel="alternate" hreflang="x-default" href="http://<?=$HTTP_HOST; ?>/">
-<link rel="alternate" hreflang="en" href="http://<?=$HTTP_HOST; ?>/en/">
-<link rel="alternate" hreflang="zh" href="http://<?=$HTTP_HOST; ?>/zh/">
+<link rel="alternate" hreflang="x-default" href="http://<?=$HTTP_HOST; ?>/<?=$global_page; ?>">
+<link rel="alternate" hreflang="en" href="http://<?=$HTTP_HOST; ?>/en/<?=$global_page; ?>">
+<link rel="alternate" hreflang="zh" href="http://<?=$HTTP_HOST; ?>/zh/<?=$global_page; ?>">
